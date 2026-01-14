31f1c634fe010557b09e237a9159efc17e2d2a3a
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-01-14 15:15:00 +0900

    #596409 [アプリ開発①]プロジェクト・基本クラスの作成
    Program クラス、StudentScore クラス、ScoreManager クラスの各基本クラスをそれぞれ Program.cs、StudentScore.cs、 ScoreManager.cs に分離して定義しました。
    本ソリューションで未使用の using ディレクティブを削除しました。
    StudentScore および ScoreManager クラスにおいて、class キーワードの後の空白を1つに修正しました。
    コードコメントをコーディング規約に従い、XML 形式のヘッダコメントに修正しました。
    Program クラスに記述していたコードコメントを削除しました。

--- ScoreManager/ScoreManager/Program.cs
+++ ScoreManager/ScoreManager/Program.cs
@@ -1,38 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScoreManager {
-    // Program「ユーザーとのやり取り」だけを担当。実際の処理はScoreManagerに任せる。
+﻿namespace ScoreManager {
     public class Program {
         static void Main(string[] args) {
         }
     }
-
-    // StudentScore「データを保持するだけ」のクラス。名前、科目、点数のデータを保持。コンストラクタを作成。
-    public class  StudentScore {
-        // 名前
-        public string Name { get; private set; }
-        
-        // 科目
-        public string Subject { get; private set; }
-
-        // 点数
-        public int Score { get; private set; }
-
-        // コンストラクタ
-        public StudentScore(string vName, string vSubject, int vScore) {
-            this.Name = vName;
-            this.Subject = vSubject;
-            this.Score = vScore;
-        }
-
-    }
-
-    // ScoreManager「データを扱う処理の集合」。CSV読み込み、表示、集計、出力などを担当。
-    public class  ScoreManager {
-        
-    }
 }
+
+
---
+++ ScoreManager/ScoreManager/ScoreManager.cs
@@ -0,0 +1,8 @@
+﻿namespace ScoreManager {
+    /// <summary>
+    /// データを処理するクラス。CSV読み込み、表示、集計、出力などを担当。
+    /// </summary>
+    public class ScoreManager {
+
+    }
+}
