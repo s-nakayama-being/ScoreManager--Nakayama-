bf6782cee473497f3fed4d9e609347cc70fed755
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-01-21 16:05:24 +0900

    #596429 [アプリ開発①]CSV読み込み機能の追加
    Program.cs内に定義しているCSVファイルパス（C_FilePath）についてコメントを追加しました。

--- ScoreManager/ScoreManager/Program.cs
+++ ScoreManager/ScoreManager/Program.cs
@@ -1,6 +1,9 @@
 ﻿namespace ScoreManager {
     public class Program {
 
+        /// <summary>
+        /// 今回読み取るCSVファイルのパス
+        /// </summary>
         private const string C_FilePath = "scores.csv";
         static void Main(string[] args) {
             var wManager = new ScoreManager();
