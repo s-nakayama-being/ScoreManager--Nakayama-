1802e1c118225af045605490265c89fdc2de424a
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-01-23 11:11:57 +0900

    #596439 [アプリ開発①]成績一覧表示機能の追加
    以下の通り修正いたしました。
    ・「が見つかりません」の後の不要なコロンを削除。
    ・メソッド名と引数の間の不要な空白を削除。
    ・CreateStudentFromLineメソッドを削除
    ・Matchメソッドを用いたデータの取り込みとオブジェクト生成を行う処理をCSV読み込みメソッド内に追加。
    ・メソッド名をReadCsvからLoadCsvに変更。
    ・読み込み対象のCSVファイルをGit追跡対象の階層へ移動し、コード内にて対象のファイルパスを相対パスで指定。

--- ScoreManager/ScoreManager/Program.cs
+++ ScoreManager/ScoreManager/Program.cs
@@ -4,12 +4,12 @@
         /// <summary>
         /// 処理対象となるCSVファイルのパス
         /// </summary>
-        private const string C_FilePath = "scores.csv";
+        private const string C_FilePath = "../../scores.csv";
         static void Main(string[] args) {
             var wManager = new ScoreManager();
 
-            wManager.ReadCsv(C_FilePath); 
-            
+            wManager.LoadCsv(C_FilePath);
+
             wManager.DisplayScores();
         }
     }
